7de2b7205582cbd0dd9c27ab9d4e49bbddf140f9
Matwa <mateowatata@gmail.com>
2026-01-30 21:41:22 -0500

    Added add Row

--- src/main.zig
+++ src/main.zig
@@ -3,27 +3,20 @@ const iguanas = @import("iguanas");
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
-    const df = try iguanas.parseCsv(allocator, "src/test.csv", &[_]iguanas.Dtype{ .string, .int, .string });
+    var df = try iguanas.parseCsv(allocator, "src/test.csv", &[_]iguanas.Dtype{ .string, .int, .string });
+    df.print(null);
 
-    var it = df.columns.iterator();
-    const first_col = it.next().?.value_ptr.*;
-    const row_count = switch (first_col) {
-        .int => |x| x.items.len,
-        .float => |x| x.items.len,
-        .string => |x| x.items.len,
-    };
+    const mateo_name = try allocator.dupe(u8, "Mateo");
+    const bogota = try allocator.dupe(u8, "Bogota");
+    try df.addRow(&[_]iguanas.Value{
+        iguanas.stringValue(mateo_name),
+        iguanas.intValue(18),
+        iguanas.stringValue(bogota),
+    });
 
-    for (0..row_count) |row_idx| {
-        var col_it = df.columns.iterator();
-        var col_idx: usize = 0;
-        while (col_it.next()) |entry| : (col_idx += 1) {
-            if (col_idx > 0) std.debug.print(", ", .{});
-            switch (entry.value_ptr.*) {
-                .int => |x| std.debug.print("{}", .{x.items[row_idx]}),
-                .float => |x| std.debug.print("{}", .{x.items[row_idx]}),
-                .string => |x| std.debug.print("{s}", .{x.items[row_idx]}),
-            }
-        }
-        std.debug.print("\n", .{});
-    }
+    df.print(null);
+
+    df.deinit();
+    const leak = gpa.deinit();
+    std.debug.print("Leaked: {b}", .{leak});
 }
--- src/root.zig
+++ src/root.zig
@@ -1,22 +1,39 @@
 const std = @import("std");
 
 pub const Dtype = enum { int, float, string };
-const Value = union(Dtype) {
+pub const Value = union(Dtype) {
     int: i32,
     float: f32,
     string: []const u8,
 };
 
-const Column = union(enum) {
-    int: std.ArrayList(i32),
-    float: std.ArrayList(f32),
-    string: std.ArrayList([]const u8),
+pub fn intValue(v: i32) Value {
+    return .{ .int = v };
+}
 
-    fn deinit(col: *Column) void {
+pub fn floatValue(v: f32) Value {
+    return .{ .float = v };
+}
+
+pub fn stringValue(v: []const u8) Value {
+    return .{ .string = v };
+}
+
+const Column = union(enum) {
+    int: std.ArrayListAligned(i32, null),
+    float: std.ArrayListAligned(f32, null),
+    string: std.ArrayListAligned([]const u8, null),
+
+    fn deinit(col: *Column, allocator: std.mem.Allocator) void {
         switch (col.*) {
-            .int => |*x| x.deinit(),
-            .float => |*x| x.deinit(),
-            .string => |*x| x.deinit(),
+            .int => |*x| x.deinit(allocator),
+            .float => |*x| x.deinit(allocator),
+            .string => |*x| {
+                for (x.items) |str| {
+                    allocator.free(str);
+                }
+                x.deinit(allocator);
+            },
         }
     }
 
@@ -40,12 +57,59 @@ const DataFrame = struct {
         };
     }
 
-    fn deinit(self: *DataFrame) void {
+    pub fn deinit(self: *DataFrame) void {
         var it = self.columns.iterator();
         while (it.next()) |entry| {
-            entry.value_ptr.deinit();
+            entry.value_ptr.deinit(self.allocator);
         }
         self.columns.deinit();
+    }
+
+    pub fn addRow(self: *DataFrame, values: []const Value) !void {
+        var col_ptrs = try self.allocator.alloc(*Column, self.columns.count());
+        defer self.allocator.free(col_ptrs);
+
+        {
+            var it = self.columns.iterator();
+            var i: usize = 0;
+            while (it.next()) |entry| : (i += 1) {
+                col_ptrs[i] = entry.value_ptr;
+            }
+        }
+
+        for (values, 0..) |val, i| {
+            try col_ptrs[i].append(self.allocator, val);
+        }
+    }
+
+    pub fn print(self: *DataFrame, rows: ?usize) void {
+        var it = self.columns.iterator();
+        const first_col = it.next().?.value_ptr.*;
+        var row_count = switch (first_col) {
+            .int => |x| x.items.len,
+            .float => |x| x.items.len,
+            .string => |x| x.items.len,
+        };
+
+        if (rows) |rownum| {
+            row_count = rownum;
+        }
+
+        for (0..row_count) |row_idx| {
+            var col_it = self.columns.iterator();
+            var col_idx: usize = 0;
+            while (col_it.next()) |entry| : (col_idx += 1) {
+                if (col_idx > 0) std.debug.print(", ", .{});
+                switch (entry.value_ptr.*) {
+                    .int => |x| std.debug.print("{}", .{x.items[row_idx]}),
+                    .float => |x| std.debug.print("{}", .{x.items[row_idx]}),
+                    .string => |x| std.debug.print("{s}", .{x.items[row_idx]}),
+                }
+            }
+            std.debug.print("\n", .{});
+        }
+
+        std.debug.print("\n", .{});
     }
 };
 
@@ -63,11 +127,13 @@ pub fn parseCsv(allocator: std.mem.Allocator, path: []const u8, types: []const D
     var header_it = std.mem.tokenizeSequence(u8, headers, ",");
     var col_idx: usize = 0;
     while (header_it.next()) |header| {
-        try df.columns.put(header, switch (types[col_idx]) {
-            .int => Column{ .int = std.ArrayList(i32){} },
-            .float => Column{ .float = std.ArrayList(f32){} },
-            .string => Column{ .string = std.ArrayList([]const u8){} },
-        });
+        var col: Column = undefined;
+        switch (types[col_idx]) {
+            .int => col = .{ .int = .{ .items = &[_]i32{}, .capacity = 0 } },
+            .float => col = .{ .float = .{ .items = &[_]f32{}, .capacity = 0 } },
+            .string => col = .{ .string = .{ .items = &[_][]const u8{}, .capacity = 0 } },
+        }
+        try df.columns.put(header, col);
         col_idx += 1;
     }
 
