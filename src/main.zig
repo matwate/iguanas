c7ece6076cc84f21da7f062195ec8d91faab86a9
Matwa <mateowatata@gmail.com>
2026-01-30 20:10:58 -0500

    Generic Schema example

--- src/main.zig
+++ src/main.zig
@@ -3,14 +3,27 @@ const iguanas = @import("iguanas");
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
+    const df = try iguanas.parseCsv(allocator, "src/test.csv", &[_]iguanas.Dtype{ .string, .int, .string });
 
-    const values = try iguanas.parseCsv(allocator, "src/test.csv");
-    const slice = values.slice();
-    const names = slice.items(.name);
-    const ages = slice.items(.age);
-    const cities = slice.items(.city);
+    var it = df.columns.iterator();
+    const first_col = it.next().?.value_ptr.*;
+    const row_count = switch (first_col) {
+        .int => |x| x.items.len,
+        .float => |x| x.items.len,
+        .string => |x| x.items.len,
+    };
 
-    for (names, ages, cities) |n, a, c| {
-        std.debug.print("{s} {d} {s}\n", .{ n, a, c });
+    for (0..row_count) |row_idx| {
+        var col_it = df.columns.iterator();
+        var col_idx: usize = 0;
+        while (col_it.next()) |entry| : (col_idx += 1) {
+            if (col_idx > 0) std.debug.print(", ", .{});
+            switch (entry.value_ptr.*) {
+                .int => |x| std.debug.print("{}", .{x.items[row_idx]}),
+                .float => |x| std.debug.print("{}", .{x.items[row_idx]}),
+                .string => |x| std.debug.print("{s}", .{x.items[row_idx]}),
+            }
+        }
+        std.debug.print("\n", .{});
     }
 }
--- src/root.zig
+++ src/root.zig
@@ -1,10 +1,56 @@
 const std = @import("std");
 
-pub fn parseCsv(allocator: std.mem.Allocator, path: []const u8) !std.MultiArrayList(Schema) {
-    // Returns an OWNED std.MultiArrayList
-    //
-    var buf: [4096]u8 = undefined;
+pub const Dtype = enum { int, float, string };
+const Value = union(Dtype) {
+    int: i32,
+    float: f32,
+    string: []const u8,
+};
 
+const Column = union(enum) {
+    int: std.ArrayList(i32),
+    float: std.ArrayList(f32),
+    string: std.ArrayList([]const u8),
+
+    fn deinit(col: *Column) void {
+        switch (col.*) {
+            .int => |*x| x.deinit(),
+            .float => |*x| x.deinit(),
+            .string => |*x| x.deinit(),
+        }
+    }
+
+    fn append(col: *Column, allocator: std.mem.Allocator, value: Value) !void {
+        switch (col.*) {
+            .int => |*x| try x.append(allocator, value.int),
+            .float => |*x| try x.append(allocator, value.float),
+            .string => |*x| try x.append(allocator, value.string),
+        }
+    }
+};
+
+const DataFrame = struct {
+    columns: std.StringArrayHashMap(Column),
+    allocator: std.mem.Allocator,
+
+    fn init(allocator: std.mem.Allocator) DataFrame {
+        return .{
+            .columns = std.StringArrayHashMap(Column).init(allocator),
+            .allocator = allocator,
+        };
+    }
+
+    fn deinit(self: *DataFrame) void {
+        var it = self.columns.iterator();
+        while (it.next()) |entry| {
+            entry.value_ptr.deinit();
+        }
+        self.columns.deinit();
+    }
+};
+
+pub fn parseCsv(allocator: std.mem.Allocator, path: []const u8, types: []const Dtype) !DataFrame {
+    var buf: [4096]u8 = undefined;
     const file = try std.fs.cwd().openFile(path, .{});
     defer file.close();
 
@@ -12,58 +58,52 @@ pub fn parseCsv(allocator: std.mem.Allocator, path: []const u8) !std.MultiArrayL
     var reader = &huh.interface;
 
     const headers = (try reader.takeDelimiter('\n')).?;
-    std.debug.print("{s}\n", .{headers});
+    var df = DataFrame.init(allocator);
 
-    var values = std.MultiArrayList(Schema){};
-    const types = [_]Dtype{ .string, .int, .string };
-
-    while (try reader.takeDelimiter('\n')) |line| {
-        var fields = std.ArrayList([]const u8){};
-        var tokens = std.mem.tokenizeSequence(u8, line, ",");
-        while (tokens.next()) |token| {
-            try fields.append(allocator, token);
-        }
-
-        var parsedValues = std.ArrayList(Value){};
-        defer parsedValues.deinit(allocator);
-
-        for (fields.items, types) |value, dtype| {
-            switch (dtype) {
-                .int => {
-                    const parsed = try std.fmt.parseInt(i32, value, 10);
-                    try parsedValues.append(allocator, Value{ .int = parsed });
-                },
-                .float => {
-                    const parsed = try std.fmt.parseFloat(f32, value);
-                    try parsedValues.append(allocator, Value{ .float = parsed });
-                },
-                .string => {
-                    try parsedValues.append(allocator, Value{ .string = value });
-                },
-            }
-        }
-
-        try values.append(allocator, .{
-            .name = parsedValues.items[0].string,
-            .age = @intCast(parsedValues.items[1].int),
-            .city = parsedValues.items[2].string,
+    var header_it = std.mem.tokenizeSequence(u8, headers, ",");
+    var col_idx: usize = 0;
+    while (header_it.next()) |header| {
+        try df.columns.put(header, switch (types[col_idx]) {
+            .int => Column{ .int = std.ArrayList(i32){} },
+            .float => Column{ .float = std.ArrayList(f32){} },
+            .string => Column{ .string = std.ArrayList([]const u8){} },
         });
+        col_idx += 1;
     }
 
-    return values;
+    var col_ptrs = try allocator.alloc(*Column, df.columns.count());
+    defer allocator.free(col_ptrs);
+
+    {
+        var it = df.columns.iterator();
+        var i: usize = 0;
+        while (it.next()) |entry| : (i += 1) {
+            col_ptrs[i] = entry.value_ptr;
+        }
+    }
+
+    while (try reader.takeDelimiter('\n')) |line| {
+        var row_it = std.mem.tokenizeSequence(u8, line, ",");
+        col_idx = 0;
+        while (row_it.next()) |token| {
+            const value: Value = switch (types[col_idx]) {
+                .int => Value{ .int = try std.fmt.parseInt(i32, token, 10) },
+                .float => Value{ .float = try std.fmt.parseFloat(f32, token) },
+                .string => Value{ .string = try allocator.dupe(u8, token) },
+            };
+
+            try col_ptrs[col_idx].append(allocator, value);
+            col_idx += 1;
+        }
+    }
+
+    std.debug.print("Column names: [", .{});
+    var it = df.columns.iterator();
+    while (it.next()) |entry| {
+        std.debug.print("{s} ", .{entry.key_ptr.*});
+    }
+
+    std.debug.print("]\n", .{});
+
+    return df;
 }
-
-// Expected headers types
-
-const Schema = struct {
-    name: []const u8,
-    age: usize,
-    city: []const u8,
-};
-
-const Dtype = enum { int, float, string };
-const Value = union(Dtype) {
-    int: i32,
-    float: f32,
-    string: []const u8,
-};
