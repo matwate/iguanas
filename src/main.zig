d1626ca9e966bf00cd899224f3b97a8b906cc3da
Matwa <mateowatata@gmail.com>
2026-02-02 16:15:01 -0500

    TrainTestSplit

--- src/main.zig
+++ src/main.zig
@@ -2,58 +2,5 @@ const std = @import("std");
 const iguanas = @import("iguanas");
 const String = iguanas.String;
 pub fn main() !void {
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
-    const allocator = arena.allocator();
-
-    const Schema = struct {
-        name: String,
-        age: usize,
-        city: String,
-        pub fn format(
-            self: @This(),
-            writer: *std.Io.Writer,
-        ) std.Io.Writer.Error!void {
-            try writer.print("{s}, {d}, {s}", .{ self.name.str(), self.age, self.city.str() });
-        }
-    };
-
-    var df = try iguanas.DataFrame(Schema).fromCsv(allocator, "src/test.csv");
-    defer df.deinit();
-
-    for (df.values.items) |*row| {
-        std.debug.print("{f}\n", .{row});
-    }
-    try df.addRow(.{
-        .name = try .init_with_contents(allocator, "Mateo"),
-        .age = 18,
-        .city = try .init_with_contents(allocator, "Bogota"),
-    });
-
-    for (df.values.items) |*row| {
-        std.debug.print("{f}\n", .{row});
-    }
-    const NewSchema = struct {
-        nameCity: String,
-
-        pub fn format(
-            self: @This(),
-            writer: *std.Io.Writer,
-        ) std.Io.Writer.Error!void {
-            try writer.print("{s}", .{self.nameCity.str()});
-        }
-    };
-
-    const transform = struct {
-        fn new(alloc: ?std.mem.Allocator, row: Schema) ?NewSchema {
-            const formatted = std.fmt.allocPrint(alloc.?, "{s}:{s}", .{ row.name.str(), row.city.str() }) catch return null;
-            return NewSchema{ .nameCity = String{ .allocator = alloc.?, .buffer = formatted, .size = formatted.len } };
-        }
-    }.new;
-    var newDf = try iguanas.DataFrame(NewSchema).fromDf(allocator, Schema, df, transform);
-    defer newDf.deinit();
-
-    for (newDf.values.items) |*row| {
-        std.debug.print("{f}\n", .{row});
-    }
+    try iguanas.inferSchema("src/KNNAlgorithmDataset.csv");
 }
--- src/root.zig
+++ src/root.zig
@@ -18,7 +18,7 @@ pub fn DataFrame(comptime Schema: type) type {
             };
         }
 
-        pub fn fromCsv(allocator: std.mem.Allocator, path: []const u8) !Self {
+        pub fn fromCsv(allocator: std.mem.Allocator, path: []const u8, hasHeader: bool) !Self {
             var buf: [4096]u8 = undefined;
             const file = try std.fs.cwd().openFile(path, .{});
             defer file.close();
@@ -30,7 +30,9 @@ pub fn DataFrame(comptime Schema: type) type {
             errdefer df.deinit();
 
             // Skip the header
-            _ = try reader.takeDelimiter('\n');
+            if (hasHeader) {
+                _ = try reader.takeDelimiter('\n');
+            }
             while (try reader.takeDelimiter('\n')) |line| {
                 var row: Schema = undefined;
                 const trimmed = std.mem.trim(u8, line, "\r");
@@ -83,5 +85,101 @@ pub fn DataFrame(comptime Schema: type) type {
             }
             self.values.deinit(self.allocator);
         }
+    };
+}
+
+pub fn inferSchema(path: []const u8) !void {
+    // Prints a possible dataframe  schema so you don't have to write it yousef.
+    var buf: [4096]u8 = undefined;
+    const file = try std.fs.cwd().openFile(path, .{});
+    defer file.close();
+
+    var huh = file.reader(&buf);
+    var reader = &huh.interface;
+
+    const header = try reader.takeDelimiter('\n');
+    const firstValue = try reader.takeDelimiter('\n');
+
+    var stdoutBuf: [4096]u8 = undefined;
+    var stdout = std.fs.File.stdout().writer(&stdoutBuf);
+
+    var writer = &stdout.interface;
+
+    try writer.print("const Schema = struct{{\n", .{});
+
+    try writer.flush();
+
+    const trimmed = std.mem.trim(u8, header.?, "\r");
+    const firstValuesTrimmed = std.mem.trim(u8, firstValue.?, "\r");
+    var header_it = std.mem.tokenizeSequence(u8, trimmed, ",");
+    var firstValues_it = std.mem.tokenizeSequence(u8, firstValuesTrimmed, ",");
+
+    while (header_it.next()) |name| {
+        const value = firstValues_it.next();
+        if (value) |val| {
+            const unquoted_name = std.mem.trim(u8, name, "\"");
+            try writer.print("\t{s} = ", .{unquoted_name});
+            // Figure out if it's an int, a float or a string as a default
+            _ = std.fmt.parseInt(i32, val, 10) catch {
+                _ = std.fmt.parseFloat(f32, val) catch {
+                    try writer.print("[]const u8,\n", .{});
+                    continue;
+                };
+                try writer.print("f32,\n", .{});
+                continue;
+            };
+            try writer.print("i32,\n", .{});
+        } else break;
+    }
+
+    try writer.print("}}\n", .{});
+    try writer.flush();
+}
+
+pub fn TrainTestSplit(comptime Schema: type, comptime LabelRemoved: type, comptime Label: type, removeLabel: *const fn (Schema) LabelRemoved, extractLabel: *const fn (Schema) Label, allocator: std.mem.Allocator, df: *DataFrame(Schema), ratio: f32) !TTSplit(Schema) {
+    var XTrain = DataFrame(LabelRemoved).init(allocator);
+    var XTest = DataFrame(LabelRemoved).init(allocator);
+    var YTrain = DataFrame(Label).init(allocator);
+    var YTest = DataFrame(Label).init(allocator);
+
+    var prng: std.Random.DefaultPrng = .init(blk: {
+        var seed: u64 = undefined;
+        try std.posix.getrandom(std.mem.asBytes(&seed));
+        break :blk seed;
+    });
+    const rand = prng.random();
+
+    for (df.values.items) |*v| {
+        const random = rand.float(f32);
+
+        if (random < ratio) {
+            const removed = removeLabel(v.*);
+            const label = extractLabel(v.*);
+
+            try XTrain.addRow(removed);
+            try YTrain.addRow(label);
+        } else {
+            const removed = removeLabel(v.*);
+            const label = extractLabel(v.*);
+
+            try XTest.addRow(removed);
+            try YTest.addRow(label);
+        }
+    }
+
+    return TTSplit(LabelRemoved, Label){
+        .XTrain = XTrain,
+        .XTest = XTest,
+        .YTrain = YTrain,
+        .YTest = YTest,
+    };
+}
+
+pub fn TTSplit(comptime LabelRemoved: type, comptime Label: type) type {
+    return struct {
+        XTrain: DataFrame(LabelRemoved),
+        XTest: DataFrame(LabelRemoved),
+        YTrain: DataFrame(Label),
+        YTest: DataFrame(Label),
     };
 }
